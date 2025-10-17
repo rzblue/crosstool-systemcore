672b0958f89f5177e14216d18a7f3dfa426689f5
Ryan Blue <ryanzblue@gmail.com>
2025-10-17 01:37:40 -0400

    Change output location to avoid moving toolchain after build

--- modify-config.sh
+++ modify-config.sh
@@ -8,4 +8,11 @@ FILE="${3:-.config}"
 CONF_LINE="$VAR=$VAL"
 
 echo "Modifying $VAR to $VAL on $FILE"
+# Try to modify existing line (either set or commented out)
 sed -E -i -e "s|($VAR)=.*|\1=$VAL|;s|# ($VAR) is not set|\1=$VAL|" "$FILE"
+
+# Check if the variable exists in the file now
+if ! grep -q "^$VAR=" "$FILE"; then
+    # Variable doesn't exist, add it
+    echo "$CONF_LINE" >> "$FILE"
+fi
